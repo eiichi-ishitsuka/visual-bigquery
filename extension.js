6a00f0b6f6904c714a824b467a85f4154098076c
eiichi-ishitsuka <eiichi-ishitsuka@bm-sms.co.jp>
2021-11-21 16:11:58 +0900

    update snipppets

--- extension.js
+++ extension.js
@@ -4,20 +4,126 @@ class SqlCompletionItemProvider {
 	constructor (){
 		this.completionItems = []
 
-		// Reserved Keywords
-		const keywords = ['ALL','AND','ANY','ARRAY','AS','ASC','ASSERT_ROWS_MODIFIED','AT','BETWEEN','BY','CASE','CAST','COLLATE','CONTAINS','CREATE','CROSS','CUBE','CURRENT','DEFAULT','DEFINE','DESC','DISTINCT','ELSE','END','ENUM','ESCAPE','EXCEPT','EXCLUDE','EXISTS','EXTRACT','FALSE','FETCH','FOLLOWING','FOR','FROM','FULL','GROUP','GROUPING','GROUPS','HASH','HAVING','IF','IGNORE','IN','INNER','INTERSECT','INTERVAL','INTO','IS','JOIN','LATERAL','LEFT','LIKE','LIMIT','LOOKUP','MERGE','NATURAL','NEW','NO','NOT','NULL','NULLS','OF','ON','OR','ORDER','OUTER','OVER','PARTITION','PRECEDING','PROTO','RANGE','RECURSIVE','RESPECT','RIGHT','ROLLUP','ROWS','SELECT','SET','SOME','STRUCT','TABLESAMPLE','THEN','TO','TREAT','TRUE','UNBOUNDED','UNION','UNNEST','USING','WHEN','WHERE','WINDOW','WITH','WITHIN','QUALIFY'];
+		// Reserved Keywords(not used snippets)
+		const keywords = [
+			'ALL',
+			'AND',
+			'ANY',
+			'ARRAY',
+			'AS',
+			'ASC',
+			'ASSERT_ROWS_MODIFIED',
+			'AT',
+			// 'BETWEEN',
+			// 'BY',
+			// 'CASE',
+			// 'CAST',
+			'COLLATE',
+			'CONTAINS',
+			// 'CREATE',
+			// 'CROSS',
+			'CUBE',
+			'CURRENT',
+			'DEFAULT',
+			'DEFINE',
+			'DESC',
+			'DISTINCT',
+			// 'ELSE',
+			// 'END',
+			'ENUM',
+			'ESCAPE',
+			'EXCEPT',
+			'EXCLUDE',
+			'EXISTS',
+			'EXTRACT',
+			'FALSE',
+			'FETCH',
+			'FOLLOWING',
+			'FOR',
+			// 'FROM',
+			// 'FULL',
+			// 'GROUP',
+			'GROUPING',
+			'GROUPS',
+			'HASH',
+			// 'HAVING',
+			'IF',
+			'IGNORE',
+			'IN',
+			// 'INNER',
+			'INTERSECT',
+			'INTERVAL',
+			'INTO',
+			'IS',
+			// 'JOIN',
+			'LATERAL',
+			// 'LEFT',
+			'LIKE',
+			// 'LIMIT',
+			'LOOKUP',
+			'MERGE',
+			'NATURAL',
+			'NEW',
+			'NO',
+			'NOT',
+			'NULL',
+			'NULLS',
+			'OF',
+			// 'ON',
+			'OR',
+			// 'ORDER',
+			// 'OUTER',
+			// 'OVER',
+			// 'PARTITION',
+			'PRECEDING',
+			'PROTO',
+			'RANGE',
+			'RECURSIVE',
+			'RESPECT',
+			// 'RIGHT',
+			'ROLLUP',
+			'ROWS',
+			// 'SELECT',
+			'SET',
+			'SOME',
+			'STRUCT',
+			'TABLESAMPLE',
+			// 'THEN',
+			'TO',
+			'TREAT',
+			'TRUE',
+			'UNBOUNDED',
+			'UNION',
+			'UNNEST',
+			'USING',
+			// 'WHEN',
+			// 'WHERE',
+			'WINDOW',
+			// 'WITH',
+			'WITHIN',
+			// 'QUALIFY'
+		];
 		for (let i = 0; i < keywords.length; i++) {
 			this.completionItems.push(
 				{
 					label: keywords[i],
 					kind: vscode.CompletionItemKind.Keyword,
-					documentation: 'Visual BigQuery - Keywords'
+					documentation: 'Visual BigQuery - Reserved Keywords'
 				}
 			)
 		}
 
 		// Other Keywords
-		const subKeywords = ['EXTERNAL_QUERY','_PARTITIONTIME','_TABLE_SUFFIX','REVOKE','GRANT','SCHEMA','DECLARE','BEGIN','CALL'];
+		const subKeywords = [
+			'EXTERNAL_QUERY',
+			'_PARTITIONTIME',
+			'_TABLE_SUFFIX',
+			'REVOKE','GRANT',
+			'SCHEMA',
+			'DECLARE',
+			'BEGIN',
+			'CALL'
+		];
 		for (let i = 0; i < subKeywords.length; i++) {
 			this.completionItems.push(
 				{
@@ -29,7 +135,21 @@ class SqlCompletionItemProvider {
 		}
 
 		// BigQuery Types
-		const types = ['INT64','NUMERIC','BIGNUMERIC','FLOAT64','BOOL','STRING','BYTES','DATE','DATETIME','TIME','TIMESTAMP','ARRAY','STRUCT'];
+		const types = [
+			'INT64',
+			'NUMERIC',
+			'BIGNUMERIC',
+			'FLOAT64',
+			'BOOL',
+			'STRING',
+			'BYTES',
+			'DATE',
+			'DATETIME',
+			'TIME',
+			'TIMESTAMP',
+			'ARRAY',
+			'STRUCT'
+		];
 		for (let i = 0; i < types.length; i++) {
 			this.completionItems.push(
 				{
