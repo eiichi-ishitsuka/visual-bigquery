4c5b8424faf386b8d31fb363cad3682f95e725ac
eiichi-ishitsuka <eiichi-ishitsuka@bm-sms.co.jp>
2021-11-24 00:18:23 +0900

    update snippets

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import {
 	CompletionItemKind,
 	CompletionList,
 	ExtensionContext,
+	MarkdownString,
 	SnippetString
 } from 'vscode';
 
@@ -21,15 +22,38 @@ class SqlCompletionItemProvider {
 		this.timezone = (workspace.getConfiguration('visual-bigquery').get('defaultTimezone') as string);
 
 		// Read snippets yaml and Register Snippets 
-		this.pushCompletionItems('keywords.yaml', CompletionItemKind.Keyword);
-		this.pushCompletionItems('subkeywords.yaml', CompletionItemKind.Property);
-		this.pushCompletionItems('types.yaml', CompletionItemKind.Field);
-		this.pushCompletionItems('functions.yaml', CompletionItemKind.Function);
-
-		this.pushCompletionItems('idioms-basic.yaml', CompletionItemKind.Snippet);
-		this.pushCompletionItems('idioms-join.yaml', CompletionItemKind.Snippet);
-		this.pushCompletionItems('idioms-window-functions.yaml', CompletionItemKind.Snippet);
-		this.pushCompletionItems('idioms-others.yaml', CompletionItemKind.Snippet);
+		this.pushCompletionItems('keywords/keywords.yaml', CompletionItemKind.Keyword);
+		this.pushCompletionItems('keywords/subkeywords.yaml', CompletionItemKind.Property);
+		
+		this.pushCompletionItems('types/types.yaml', CompletionItemKind.Field);
+		
+		this.pushCompletionItems('functions/uuid-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/aggregate-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/approximate-aggregate-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/array-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/bit-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/date-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/datetime-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/debugging-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/geography-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/hash-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/hyper-log-log-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/json-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/mathematical-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/navigation-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/net-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/numbering-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/operator-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/security-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/statistical-aggregate-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/string-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/time-functions.yaml', CompletionItemKind.Function);
+		this.pushCompletionItems('functions/timestamp-functions.yaml', CompletionItemKind.Function);
+		
+		this.pushCompletionItems('idioms/idioms-basic.yaml', CompletionItemKind.Snippet);
+		this.pushCompletionItems('idioms/idioms-join.yaml', CompletionItemKind.Snippet);
+		this.pushCompletionItems('idioms/idioms-window-functions.yaml', CompletionItemKind.Snippet);
+		this.pushCompletionItems('idioms/idioms-others.yaml', CompletionItemKind.Snippet);
 
 		this.completionList = new CompletionList(this.completionItems, false);
 	}
@@ -59,7 +83,7 @@ class SqlCompletionItemProvider {
 					kind: kind,
 					insertText: new SnippetString(yml.data[i].body.replace('{{ timezone }}', this.timezone)),
 					detail: yml.data[i].label + ' -- Visual-BigQuery ' + yml.name,
-					documentation: yml.data[i].documentation
+					documentation: new MarkdownString(yml.data[i].documentation)
 				}
 			);
 		}
